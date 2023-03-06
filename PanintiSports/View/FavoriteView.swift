d722b5bd21aa1120a47c43f2c42cebc351e7b7a2
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-03-07 01:50:52 +0700

    feat: delete favorite team

--- PanintiSports/View/FavoriteView.swift
+++ PanintiSports/View/FavoriteView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct FavoriteView: View {
+    @Environment(\.managedObjectContext) private var viewContext
     @StateObject var favoriteVM = FavoriteViewModel()
     @FetchRequest(entity: Favorite.entity(), sortDescriptors: [NSSortDescriptor(key: "idTeam", ascending: true)]) var favoriteTeams: FetchedResults<Favorite>
     var body: some View {
@@ -15,34 +16,24 @@ struct FavoriteView: View {
             if favoriteTeams.isEmpty {
                 Text("No Favorite Team")
             } else {
-                if favoriteVM.listTypeGrid {
-                    ScrollView {
-                        LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))], spacing: 10) {
-                            ForEach(favoriteTeams, id: \.self) { team in
-                                VStack {
-                                    AsyncImage(url: URL(string: team.strTeamBadge ?? "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"),
-                                               content: { image in
-                                                   image.resizable()
-                                                       .aspectRatio(contentMode: .fit)
-                                                       .frame(maxWidth: 100, maxHeight: 100)
-                                               },
-                                               placeholder: {
-                                                   ProgressView()
-                                                       .frame(width: 100, height: 100)
-                                               })
-                                    Text(team.strTeam ?? "-")
-                                }.onTapGesture {
-                                    let teamDetail = Team(idTeam: team.idTeam, strTeam: team.strTeam, strLeague: team.strLeague, strTeamBadge: team.strTeamBadge, strDescriptionEN: team.strDescriptionEN)
-                                    favoriteVM.teamDetail = teamDetail
-                                    favoriteVM.seeDetailView = true
-                                }
-                            }
+                List(favoriteTeams, id: \.self) { team in
+                    HStack {
+                        if favoriteVM.editMode {
+                            favoriteVM.selectedTeams.contains(Team(idTeam: team.idTeam, strTeam: team.strTeam, strLeague: team.strLeague, strTeamBadge: team.strTeamBadge, strDescriptionEN: team.strDescriptionEN)) ? Image(systemName: "checkmark.circle.fill") :
+                                Image(systemName: "circle")
                         }
-                        .padding()
-                    }
-                } else {
-                    List(favoriteTeams, id: \.self) { team in
-                        Text(team.strTeam ?? "-").onTapGesture {
+                        Text(team.strTeam ?? "-")
+                    }.onTapGesture {
+                        if favoriteVM.editMode {
+                            let selectedTeam = Team(idTeam: team.idTeam, strTeam: team.strTeam, strLeague: team.strLeague, strTeamBadge: team.strTeamBadge, strDescriptionEN: team.strDescriptionEN)
+                            if favoriteVM.selectedTeams.contains(selectedTeam) {
+                                if let index = favoriteVM.selectedTeams.firstIndex(of: selectedTeam) {
+                                    favoriteVM.selectedTeams.remove(at: index)
+                                }
+                            } else {
+                                favoriteVM.selectedTeams.append(selectedTeam)
+                            }
+                        } else {
                             let teamDetail = Team(idTeam: team.idTeam, strTeam: team.strTeam, strLeague: team.strLeague, strTeamBadge: team.strTeamBadge, strDescriptionEN: team.strDescriptionEN)
                             favoriteVM.teamDetail = teamDetail
                             favoriteVM.seeDetailView = true
@@ -56,9 +47,16 @@ struct FavoriteView: View {
         }.toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
                 Button {
-                    favoriteVM.listTypeGrid.toggle()
+                    if favoriteVM.selectedTeams.isEmpty {
+                        favoriteVM.editMode.toggle()
+                    } else {
+                        for team in favoriteVM.selectedTeams{
+                            favoriteVM.unfavoriteTeam(context: viewContext, id: team.idTeam!)
+                        }
+                    }
+                    favoriteVM.selectedTeams = [Team]()
                 } label: {
-                    Image(systemName: favoriteVM.listTypeGrid ? "list.bullet" : "square.grid.2x2")
+                    Text(favoriteVM.selectedTeams.isEmpty ? favoriteVM.editMode ? "Done" : "Edit" : "Delete").foregroundColor(favoriteVM.selectedTeams.isEmpty ? .accentColor : .red)
                 }
             }
         }
--- PanintiSports/ViewModel/FavoriteViewModel.swift
+++ PanintiSports/ViewModel/FavoriteViewModel.swift
@@ -6,10 +6,30 @@
 //
 
 import Foundation
+import CoreData
 import SwiftUI
 
 class FavoriteViewModel: ObservableObject{
     @Published var teamDetail: Team = .init(idTeam: "", strTeam: "", strLeague: "", strTeamBadge: "", strDescriptionEN: "")
     @Published var seeDetailView = false
-    @Published var listTypeGrid = false
+    @Published var editMode = false
+    @Published var selectedTeams = [Team]()
+    
+    func unfavoriteTeam(context: NSManagedObjectContext, id: String) {
+        let fetchRequest: NSFetchRequest<Favorite> = Favorite.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "idTeam == %@", id)
+        do {
+            let firstFavorite = try context.fetch(fetchRequest)
+            context.delete(firstFavorite[0])
+            save(context: context)
+        } catch {
+            fatalError("Uh, fetch problem...")
+        }
+    }
+    
+    func save(context: NSManagedObjectContext) {
+        do {
+            try context.save()
+        } catch {}
+    }
 }
