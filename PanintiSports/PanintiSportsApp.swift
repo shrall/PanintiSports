05a9fd7d3239dee2af93cea508ed02dfb1e63e9c
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-03-06 15:02:35 +0700

    Initial Commit

---
+++ PanintiSports/PanintiSportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanintiSportsApp.swift
+//  PanintiSports
+//
+//  Created by Marshall Kurniawan on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PanintiSportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
